411394ff411435fa47f353452f8a49d0845ab10c
leojudya <30649532+leojudya@users.noreply.github.com>
2020-09-17 20:12:12 +0800

    Add files via upload

--- websocket/wsserver.py
+++ websocket/wsserver.py
@@ -8,13 +8,14 @@ from psutil._common import bytes2human
     
 async def hello(websocket, path):
     while True:
+        t = time.strftime("%Y-%M-%d %H:%M:%S", time.localtime())
+        await websocket.send(t)
         cpu_percent = "CPU: " + str(psutil.cpu_percent()) + "%"
         await websocket.send(cpu_percent)
         vir = str(psutil.virtual_memory().percent)
         swap = str(psutil.swap_memory().percent)
         await websocket.send(vir)
         await websocket.send(swap)
-        await websocket.send("End")
         for part in psutil.disk_partitions(all=False):
             if os.name == 'nt':
                 if 'cdrom' in part.opts or part.fstype == '':
@@ -24,11 +25,8 @@ async def hello(websocket, path):
                     continue
             usage = psutil.disk_usage(part.mountpoint)
             await websocket.send(f"{part.device} {int(usage.percent)}")
-        temps = psutil.sensors_temperatures()
-        for name, entries in temps.items():
-            await websocket.send("    %-20s %s °C (high = %s °C, critical = %s °C)" % (
-                entry.label or name, entry.current, entry.high,
-                entry.critical))
+        await websocket.send("End")
+
         await asyncio.sleep(1)
 
     
